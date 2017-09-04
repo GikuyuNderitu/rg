6d089e146e6bcf34ce04b73ec29f54b297eccb5c
Gikuyu Nderitu <gikyunderitu@gmail.com>
2017-09-04 17:26:20 -0400

    Update changes from merge

--- cmd/container.go
+++ cmd/container.go
@@ -111,8 +111,4 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// containerCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-<<<<<<< HEAD
-
-=======
->>>>>>> 798e32668d41dbdeceb61fe49fb302b9266f8479
 }
